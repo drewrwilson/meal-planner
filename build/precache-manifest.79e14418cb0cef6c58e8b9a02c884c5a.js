d8039cbebe586847b9d45bc1799fa76ab2c5f59b
Drew <drew.r.wilson@gmail.com>
2019-08-30 12:55:43 -0400

    add a production version of the appa

---
+++ build/precache-manifest.79e14418cb0cef6c58e8b9a02c884c5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb6f211ed29b0059b3a8ba3c066a296",
+    "url": "/meal-planner/index.html"
+  },
+  {
+    "revision": "a04e0a631aed3f84a40b",
+    "url": "/meal-planner/static/css/main.1b5682d0.chunk.css"
+  },
+  {
+    "revision": "94e3695a4fbf5ee44e46",
+    "url": "/meal-planner/static/js/2.8471bea0.chunk.js"
+  },
+  {
+    "revision": "a04e0a631aed3f84a40b",
+    "url": "/meal-planner/static/js/main.280633d7.chunk.js"
+  },
+  {
+    "revision": "fd5fbe15959defad88c7",
+    "url": "/meal-planner/static/js/runtime~main.c0f4192b.js"
+  }
+]);
